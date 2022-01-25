0906ce7ee66dde6e48a043afdabb28f9f2a3af41
Mubarak <imubarak2424@gmail.com>
2022-01-25 23:37:17 +0100

    fix footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="fixed-bottom bg-dark py-2">
-    <div className="container">
-      <p className="text-white text-center">
-        Copyright &copy; 2022 designed by Mubarak A Ibrahim and Tumworobere Annah
-      </p>
-    </div>
-  </footer>
+  <div className="footers">
+    <footer className="bg-dark py-2">
+      <div className="container">
+        <p className="text-white text-center">
+          Copyright &copy; 2022 designed by Mubarak A Ibrahim and Tumworobere Annah
+        </p>
+      </div>
+    </footer>
+  </div>
 );
 
 export default Footer;
