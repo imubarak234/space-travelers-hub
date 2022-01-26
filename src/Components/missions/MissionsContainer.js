7f9a75cbfd58535b4ff58a28ef01f30d7f52de66
Mubarak <imubarak2424@gmail.com>
2022-01-26 01:16:50 +0100

    reserve mission

--- src/Components/missions/MissionsContainer.js
+++ src/Components/missions/MissionsContainer.js
@@ -1,13 +1,24 @@
+/* eslint-disable no-console */
+
 import React from 'react';
-import { useSelector, shallowEqual } from 'react-redux';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import MissionsGrid from './MissionsGrid';
+import { reserveMission } from '../../redux/Missions/Missions';
 
 const MissionsContainer = () => {
+  const dispatch = useDispatch();
   const states = useSelector((state) => state.missionsReducer, shallowEqual);
+
+  const bookMission = (id) => {
+    dispatch(reserveMission(id));
+  };
 
   return (
     <div className="container-fluid h-auto py-5">
-      <MissionsGrid list={states} />
+      <MissionsGrid
+        list={states}
+        booking={bookMission}
+      />
     </div>
   );
 };
--- src/Components/missions/MissionsGrid.js
+++ src/Components/missions/MissionsGrid.js
@@ -3,7 +3,7 @@ import MissionsItems from './MissionsItems';
 import './Mission.css';
 
 const MissionsGrid = (props) => {
-  const { list } = props;
+  const { list, booking } = props;
   let count = 0;
 
   const changes = (next) => {
@@ -13,8 +13,22 @@ const MissionsGrid = (props) => {
     return ans;
   };
 
+  const buttonClass = (args) => {
+    let ans = '';
+    if (args) ans = 'btn btn-outline-danger';
+    else ans = 'btn btn-outline-secondary';
+    return ans;
+  };
+
+  const buttonInfo = (args) => {
+    let ans = '';
+    if (args) ans = 'Leave Mission';
+    else ans = 'Join Mission';
+    return ans;
+  };
+
   return (
-    <div className="container py-3">
+    <div className="px-3 py-3">
       <div className="row">
         <div className="col-2 bords">
           <p className="h3">Mission</p>
@@ -34,9 +48,13 @@ const MissionsGrid = (props) => {
           return (
             <MissionsItems
               key={missions.mission_id}
+              ids={missions.mission_id}
               name={missions.mission_name}
               description={missions.description}
               classes={changes(count)}
+              booking={booking}
+              infos={buttonInfo(missions.reserved)}
+              btnClass={buttonClass(missions.reserved)}
             />
           );
         })
@@ -52,6 +70,7 @@ MissionsGrid.propTypes = {
       description: PropTypes.string,
     }),
   ).isRequired,
+  booking: PropTypes.func.isRequired,
 };
 
 export default MissionsGrid;
