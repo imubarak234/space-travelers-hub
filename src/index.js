38bce51a378e18421f79b3a20ad7190bd66953d3
Tumworobere <annaht.annah@gmail.com>
2022-01-27 00:41:32 +0300

    fix errors

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Footer from './Components/Footer';
 import store from './redux/configureStore';
 import MissionsContainer from './Components/missions/MissionsContainer';
 import ProfileContainer from './Components/profiles/ProfileContainer';
+import RocketContainer from './Components/rockets/RocketContainer';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -15,6 +16,7 @@ ReactDOM.render(
       <Router>
         <Naver />
         <Routes>
+          <Route path="/" element={<RocketContainer />} />
           <Route path="/missions" element={<MissionsContainer />} />
           <Route path="/my-profile" element={<ProfileContainer />} />
         </Routes>
@@ -24,7 +26,6 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
